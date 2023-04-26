ab4c3c1b45c43858e2f93a6be8ce1a1b6dd3af60
Volki23 <sertkayavolkan@hotmail.com>
2023-04-26 21:00:13 +0200

    V2

--- src/AtmProject.java
+++ src/AtmProject.java
@@ -3,7 +3,8 @@ public class AtmProject {
     public static void main(String[] args) {
         System.out.println("Bu bir Atm Projesidir...");
     //Version 1
-
+        System.out.println("Java is easy...");
+        //Version 2
 
 
     }//main
